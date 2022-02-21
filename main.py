f33693d58c2a70104f8a64a09431da766ea0c336
ryman1 <ryman1@gmail.com>
2022-02-21 13:04:35 -0700

    environ

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import socket
+import os
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+    addr1 = socket.gethostbyname('google.com')
+    print(f'{addr1}')
+    print(f'{os.environ.items}')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
