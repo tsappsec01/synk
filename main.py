70e1a5583c0333631d7e2ea7dfee3e9e68d72209
ryman1 <ryman1@gmail.com>
2022-02-23 10:06:37 -0700

    egress

--- main.py
+++ main.py
@@ -9,19 +9,51 @@ import requests
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    # addr1 = socket.gethostbyname('wyycjvaegwxg6336qvo3o2i6jxpndc.burpcollaborator.net')
-    # print(f'{addr1}')
-    csrftoken = 'A7MvkJGg-XWQjGC7i8bwprpV_6NDI3nGcJpc'
+    # addr1 = socket.gethostbyname('uywfveb7x5xhwlfpz69352xvrmxcl1.burpcollaborator.net')
+    # print(addr1)
+    # # print(f'{addr1}')
+    # csrftoken = 'A7MvkJGg-XWQjGC7i8bwprpV_6NDI3nGcJpc'
+    #
+    # r = requests.post('https://app.snyk.io/org/tsappsec01/manage/settings/ignore',
+    #                   data={'_csrf': f'{csrftoken}',
+    #                         'orgIgnoreSettings[adminOnly]': 'false',
+    #                         'orgIgnoreSettings[reasonRequired]': 'true'},
+    #                   cookies={'snyk.id': 's:AA51MO2v8zPib5yXGsL7zECapsSe7kM0.tvBtH477nieEzAyMXOl8/M4w13iiNAWagMkXJtLUiTk'})
+    # print(r.text)
+    # # for i in r.cookies.items():
+    # #     print(i)r.cookies.items()[0])
+    # print(r.status_code)
+    TCP_IP = '72.14.183.78'
+    # TCP_IP = socket.gethostbyname('uywfveb7x5xhwlfpz69352xvrmxcl1.burpcollaborator.net')
+    TCP_PORT = 8089
+    BUFFER_SIZE = 1024
+    MESSAGE = "Hello, World!"
+    startport = 8075
+    endport = 8090
+    openports = []
 
-    r = requests.post('https://app.snyk.io/org/tsappsec01/manage/settings/ignore',
-                      data={'_csrf': f'{csrftoken}',
-                            'orgIgnoreSettings[adminOnly]': 'false',
-                            'orgIgnoreSettings[reasonRequired]': 'true'},
-                      cookies={'snyk.id': 's:AA51MO2v8zPib5yXGsL7zECapsSe7kM0.tvBtH477nieEzAyMXOl8/M4w13iiNAWagMkXJtLUiTk'})
-    print(r.text)
-    # for i in r.cookies.items():
-    #     print(i)r.cookies.items()[0])
-    print(r.status_code)
+
+    for port in range(startport, endport+1):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+            s.connect((TCP_IP, port))
+        except ConnectionRefusedError:
+            s.close()
+            continue
+        except socket.timeout:
+            print(f'{port} timeout')
+            continue
+        openports.append(port)
+        s.close()
+
+    for port in openports:
+        print(port)
+    # s.send(MESSAGE)
+    # data = s.recv(BUFFER_SIZE)
+    # s.close()
+    # print
+    # "received data:", data
 
 
 # Press the green button in the gutter to run the script.
